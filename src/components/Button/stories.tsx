b33d66b06715e442d95b8ba0a2e8ce5dce2576b1
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-08-02 16:45:05 -0300

    create style for Button comp

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,9 +1,18 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Button from '.'
+import Button, { ButtonProps } from '.'
 
 export default {
   title: 'Button',
-  component: Button
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <Button />
+export const Default: Story<ButtonProps> = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy Now'
+}
