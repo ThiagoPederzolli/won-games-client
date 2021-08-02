b24618e6e7476bf43e025455324633c12f64cea9
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-08-02 14:46:18 -0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,31 @@
+// module.exports = {
+//   testEnvironment: 'jsdom',
+//   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+//   collectCoverage: true,
+//   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+//   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+//   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+// }
+
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/utils/apollo.ts',
+    '!src/utils/apolloCache.ts',
+    '!src/types/**/*.d.ts',
+    '!src/graphql/**/*.ts',
+    '!src/**/mock.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  }
 }
