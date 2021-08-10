f1cd0387c1777a81af6b5225f5b8f13d6f1aa0bb
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-08-10 09:32:51 -0300

    created logic and tests to open/close menu on mobile

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -34,3 +34,11 @@ export const MenuGroup = styled.div`
     }
   `}
 `
+type MenuFullProps = {
+  isOpen: boolean
+}
+export const MenuFull = styled.nav<MenuFullProps>`
+  ${({ isOpen }) => css`
+    opacity: ${isOpen ? 1 : 0};
+  `}
+`
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import Menu from '.'
 
@@ -11,5 +11,26 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/won games/i)).toBeInTheDocument()
     // expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o MenuFullScreen
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // verificar se o menu está escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // clicar para abrir e verificar se abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // clicar para fechar e verificar se fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
