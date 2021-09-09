312ad61342e92a8f54db53b3cc2a6c46af654bd2
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-09-09 15:24:07 -0300

    Added props size and lineColor to Heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper lineLeft={lineLeft} color={color} lineBottom={lineBottom}>
+  <S.Wrapper
+    lineLeft={lineLeft}
+    lineColor={lineColor}
+    color={color}
+    lineBottom={lineBottom}
+    size={size}
+  >
     {children}
   </S.Wrapper>
 )
