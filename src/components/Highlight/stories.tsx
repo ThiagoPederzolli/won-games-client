11472c3e927c6710513e5ab7b6b78424da7c46d7
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-09-15 11:08:19 -0300

    added float image to Highlight comp with test

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -5,7 +5,7 @@ export default {
   title: 'Highlight',
   component: Highlight,
   args: {
-    title: 'Read Dead está de de volta!',
+    title: 'Red Dead está de volta!',
     subtitle: 'Venha conhecer as novas aventuras de John Marston',
     backgroundImage: '/img/red-dead-img.png',
     buttonLabel: 'Buy now',
@@ -13,4 +13,18 @@ export default {
   }
 } as Meta
 
-export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -2,8 +2,32 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 import { HighlightProps } from '.'
 
-export const Wrapper = styled.section<Pick<HighlightProps, 'backgroundImage'>>`
-  ${({ theme, backgroundImage }) => css`
+const wrapperModifier = {
+  right: () => css`
+    grid-template-areas: 'floatImage content';
+    grid-template-columns: 1.3fr 2fr;
+
+    ${Content} {
+      text-align: right;
+    }
+  `,
+  left: () => css`
+    grid-template-areas: 'content floatImage';
+    grid-template-columns: 2fr 1.3fr;
+
+    ${Content} {
+      text-align: left;
+    }
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+}
+
+type WrapperProps = Pick<HighlightProps, 'backgroundImage' | 'alignment'>
+
+export const Wrapper = styled.section<WrapperProps>`
+  ${({ backgroundImage, alignment }) => css`
     position: relative;
     background-image: url(${backgroundImage});
     background-position: center center;
@@ -22,13 +46,29 @@ export const Wrapper = styled.section<Pick<HighlightProps, 'backgroundImage'>>`
     ${media.greaterThan('medium')`
       height: 32rem;
     `}
+
+    ${wrapperModifier[alignment!]()}
+  `}
+`
+
+export const FloatImage = styled.img`
+  ${({ theme }) => css`
+    align-self: end;
+    grid-area: floatImage;
+    z-index: ${theme.layers.base};
+    max-height: 23rem;
+    max-width: 100%;
+
+    ${media.greaterThan('medium')`
+      height: 32rem;
+    `}
   `}
 `
 
 export const Content = styled.div`
   ${({ theme }) => css`
+    grid-area: content;
     z-index: ${theme.layers.base};
-    text-align: right;
     padding: ${theme.spacings.xsmall};
 
     ${media.greaterThan('medium')`
