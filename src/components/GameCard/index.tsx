a8fe5333012ad2d57d2fbfa1b7616780ac2138d8
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-09-15 13:04:41 -0300

    added Ribbon to GameCard comp

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -5,6 +5,8 @@ import {
   FavoriteBorder
 } from '@styled-icons/material-outlined'
 import Button from 'components/Button'
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
+import { ReactNode } from 'react'
 import * as S from './styles'
 
 export type GameCardProps = {
@@ -15,6 +17,9 @@ export type GameCardProps = {
   promotionalPrice?: string
   favorite?: boolean
   onFav?: () => void
+  ribbon?: ReactNode
+  ribbonSize?: RibbonSizes
+  ribbonColor?: RibbonColors
 }
 
 const GameCard = ({
@@ -24,7 +29,10 @@ const GameCard = ({
   price,
   promotionalPrice,
   favorite = false,
-  onFav
+  onFav,
+  ribbon = '',
+  ribbonSize = 'normal',
+  ribbonColor = 'primary'
 }: GameCardProps) => (
   <S.Wrapper>
     <S.ImageBox>
@@ -48,6 +56,11 @@ const GameCard = ({
         <Button icon={<AddShoppingCart />} size="small" />
       </S.BuyBox>
     </S.Content>
+    {!!ribbon && (
+      <Ribbon size={ribbonSize} color={ribbonColor}>
+        {ribbon}
+      </Ribbon>
+    )}
   </S.Wrapper>
 )
 
