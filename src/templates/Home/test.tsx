c7049e097bee14becd132cf33d11dd1b6c59ea30
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-09-22 16:25:53 -0300

    added tests for Home Page

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,29 +1,45 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import Home from '.'
 
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+  banners: bannersMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcomingGames: [gamesMock[0]],
+  upcomingHighlight: highlightMock,
+  upcomingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighlight: highlightMock
+}
+
 describe('<Home />', () => {
-  it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+  it('should render the Home Page correctly', () => {
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /Contact/i })
     ).toBeInTheDocument()
-  })
-
-  it('should render sections', () => {
-    renderWithTheme(<Home />)
-
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /most popular/i })
     ).toBeInTheDocument()
     expect(
-      screen.getByRole('heading', { name: /upcomming/i })
+      screen.getByRole('heading', { name: /upcoming/i })
     ).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    expect(screen.getAllByText(/Red Dead está de volta!/i)).toHaveLength(3)
   })
 })
