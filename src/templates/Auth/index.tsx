70160bb9003f4033ff53e326083a3041ea6356bd
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-09-24 11:10:11 -0300

    created desktop style for SignIn and SignUp pages

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -11,19 +11,26 @@ export type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
-      <Heading>Seus jogos favoritos em um só lugar</Heading>
-      <S.Subtitle>
-        A <strong>WON</strong> é a melhor e mais completa plataforma de games.
+      <S.BannerContent>
+        <Logo />
+        <div>
+          <Heading>Seus jogos favoritos em um só lugar</Heading>
+          <S.Subtitle>
+            A <strong>WON</strong> é a melhor e mais completa plataforma de
+            games.
+          </S.Subtitle>
+        </div>
         <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
-      </S.Subtitle>
+      </S.BannerContent>
     </S.BannerBlock>
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
-      {children}
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 )
