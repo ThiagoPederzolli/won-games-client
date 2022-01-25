4bbe30ce5fb9790b592ceb512e0ed4c7c79a3fde
tsilva <thiago.silva@possible.com>
2022-01-25 17:52:58 -0300

    comp Logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,8 +1,60 @@
 import styled, { css } from 'styled-components'
+// styled-media-query é uma lib que auxilia
+// na responsividade de componentes e paginas
+// possui por padrão 4 breakpoints:
+// - huge: 1440px
+// - large: 1170px
+// - medium: 768px
+// - small: 450px
+// porém esses números podem ser sobrescritos
+// ou até mesmo criados novos breakpoints
+// com esses números, podemos usar três métodos:
+// lessThan(breakpoint ou size) - aplicará o estilo em larguras menores que.
+// greaterThan(breakpoint ou size) - aplicará o estilo em larguras maiores que.
+// between(breakpoint ou size inicial, breakpoint ou size final) - aplicará o estilo em larguras entre valor inicial e final
+import media from 'styled-media-query'
 import { LogoProps } from '.'
 
+// modifiers são métodos criados para gerar mudança de estilo de forma
+// dinâmica de acordo com o valor de prop que o componente receber
+const wrapperModifiers = {
+  normal: () => css`
+    width: 11rem;
+    height: 3.5rem;
+  `,
+  large: () => css`
+    width: 20rem;
+    height: 5.9rem;
+  `,
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
+  `
+}
+
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
+    // o uso da excalamação(non-null assertion) indica que
+    // mesmo ciente de que é um valor opcional, temos certeza
+    // que o número está sendo passado
     color: ${theme.colors[color!]};
+
+    // o uso de duas exclamações serve para o TS transformar
+    // em booleano, ou seja size deixa de ser uma string
+    // e vira um true ou false, se estivesse com 3 exclamações
+    // ele ia considerar true quando size fosse false
+    ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -5,8 +5,9 @@ import { renderWithTheme } from 'utils/tests/helpers'
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
     renderWithTheme(<Logo />)
-
-    expect(screen.getByLabelText(/won Games/i).parentElement).toHaveStyle({
+    // usamos o parentElement para ser possível acessar o elemento pai
+    // pois estamos testando a div que engloba a logo, mas conseguimos pegar só a logo
+    expect(screen.getByLabelText(/logo won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
     })
 
@@ -16,8 +17,37 @@ describe('<Logo />', () => {
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
 
-    expect(screen.getByLabelText(/won Games/i).parentElement).toHaveStyle({
+    expect(screen.getByLabelText(/logo won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render a normal size by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/logo won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a large size when size is passed', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/logo won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile is true', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    // to toHaveStyleRule é parte da lib 'jest-styled-components'
+    // o toHaveStyleRule é útil para testes de responsividade
+    // ou de mudanças de estilo mediante a ações
+    // pois ele aceita receber media-queries ou modifiers
+    expect(
+      screen.getByLabelText(/logo won Games/i).parentElement
+    ).toHaveStyleRule('width', '5.8rem', {
+      media: '(max-width: 768px)'
     })
   })
 })
