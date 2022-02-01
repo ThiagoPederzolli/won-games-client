f2f08d142f5a26e9a77a48b96d387a81ac2b76c0
tsilva <thiago.silva@possible.com>
2022-02-01 10:35:57 -0300

    removendo codigo comentado

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -22,27 +22,12 @@ const Banner = ({
   size = 'normal'
 }: BannerProps) => (
   <S.Wrapper>
-    {/* {!!ribbon && (
-      <Ribbon color={color} size={size}>
-        {ribbon}
-      </Ribbon>
-    )}
-    <S.Image src={img} role="img" aria-label={title} />
-    <S.Caption>
-      <S.Title>{title}</S.Title>
-      <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
-      <Button as="a" href={buttonLink} size="large">
-        {buttonLabel}
-      </Button>
-    </S.Caption> */}
     {!!ribbon && (
       <Ribbon color={color} size={size}>
         {ribbon}
       </Ribbon>
     )}
-
     <S.Image src={img} role="img" aria-label={title} />
-
     <S.Caption>
       <S.Title>{title}</S.Title>
       <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
