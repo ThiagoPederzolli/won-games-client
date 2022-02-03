9f526bc1e7ed613b5e0211e90fba4c45f0f8c125
tsilva <thiago.silva@possible.com>
2022-02-03 08:55:20 -0300

    Criado Sliders de banner e gamecard

---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,66 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import GameCardSlider from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+import theme from 'styles/theme'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+describe('<GameCardSlider />', () => {
+  it('should render the GameCardSlider', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the GameCardSlider with white arrows if color is passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: theme.colors.white
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: theme.colors.white
+    })
+
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+})
---
+++ src/components/Slider/test.tsx
@@ -0,0 +1,24 @@
+// o import do match-media deve ser sempre a primeira linha do arquivo
+import 'match-media-mock'
+import { render, screen } from '@testing-library/react'
+import Slider from '.'
+
+describe('<Slider />', () => {
+  it('should render the Slider', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Slide 1</p>
+        <p>Slide 2</p>
+      </Slider>
+    )
+
+    expect(
+      screen.getByText(/slide 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+    expect(
+      screen.getByText(/slide 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+})
