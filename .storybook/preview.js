950063fcb138fb3037d1f4dfdc0222738dce261a
tsilva <thiago.silva@possible.com>
2022-02-03 14:18:22 -0300

    ajustes de BG para Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,21 +3,21 @@
 // import GlobalStyles from 'styles/global'
 // import theme from 'styles/theme'
 
-// export const parameters = {
-//   backgrounds: {
-//     default: 'won-light',
-//     values: [
-//       {
-//         name: 'won-light',
-//         value: theme.colors.white
-//       },
-//       {
-//         name: 'won-dark',
-//         value: theme.colors.mainBg
-//       }
-//     ]
-//   }
-// }
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
 
 // export const decorators = [
 //   (Story, context) => (
@@ -43,7 +43,7 @@ import theme from 'styles/theme'
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyles removeBG />
       <Story />
     </ThemeProvider>
   )
