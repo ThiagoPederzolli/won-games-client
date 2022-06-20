5c4fbb05e1ae901077a31ea947e55c6b83178935
tsilva <thiago.silva@possible.com>
2022-06-20 12:50:02 -0300

    conteudo secao 21 do curso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,13 @@ export default function Index(props: HomeTemplateProps) {
 // os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
 
 // getStaticProps => gerar estático em build time (gatsby)
+// dentro do getStatic com dado existem dois cenários:
+// - getStaticProps, quando o conteúdo da página depende de dados externos
+// - getStaticPaths, quando o path da página depende de dados externos(rotas dinâmicas)
 // getServerSideProps => gerar via ssr a cada request (nunca vai para o bundle do client)
 // getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois do 1 req)
+
+// a função deve ser exportada
 export function getServerSideProps() {
   // faz lógica
   // pode ser buscar dados numa API
