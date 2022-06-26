ac1e120b68bfecee09574078d743974cf9594685
tsilva <thiago.silva@possible.com>
2022-06-26 20:04:19 -0300

    conteudo secao 28 do curso

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -27,12 +27,18 @@ const Menu = ({ username }: MenuProps) => {
       </MediaMatch>
 
       <S.LogoWrapper>
-        <Logo hideOnMobile />
+        <Link href="/" passHref>
+          <a>
+            <Logo hideOnMobile />
+          </a>
+        </Link>
       </S.LogoWrapper>
 
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
@@ -46,6 +52,9 @@ const Menu = ({ username }: MenuProps) => {
         </S.IconWrapper>
         {!username && (
           <MediaMatch greaterThan="medium">
+            {/* passando o passHref ele identificará que deve passar o link para o elemento abaixo
+            dele, porém, caso o componente filho do link se trate te um functional component, teremos
+            outra situação para abordar, será necessário implementar o forwardRef (ver component/Button) */}
             <Link href="/sign-in" passHref>
               <Button as="a">Sign in</Button>
             </Link>
@@ -56,7 +65,9 @@ const Menu = ({ username }: MenuProps) => {
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
 
           {!!username && (
@@ -69,12 +80,9 @@ const Menu = ({ username }: MenuProps) => {
 
         {!username && (
           <S.RegisterBox>
-            {/* passando o passHref ele identificará que deve passar o link para o elemento abaixo
-            dele, porém, caso o componente filho do link se trate te um functional component, teremos
-            outra situação para abordar, será necessário implementar o forwardRef (ver component/Button) */}
             <Link href="/sign-in" passHref>
               <Button fullWidth size="large" as="a">
-                Log in now
+                Sign in
               </Button>
             </Link>
             <span>or</span>
@@ -89,3 +97,92 @@ const Menu = ({ username }: MenuProps) => {
 }
 
 export default Menu
+
+// import Link from 'next/link'
+
+// import { useState } from 'react'
+// import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+// import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+// import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+// import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+
+// import Button from 'components/Button'
+// import Logo from 'components/Logo'
+// import MediaMatch from 'components/MediaMatch'
+// import * as S from './styles'
+
+// export type MenuProps = {
+//   username?: string
+// }
+
+// const Menu = ({ username }: MenuProps) => {
+//   const [isOpen, setIsOpen] = useState(false)
+
+//   return (
+//     <S.Wrapper>
+//       <MediaMatch lessThan="medium">
+//         <S.IconWrapper onClick={() => setIsOpen(true)}>
+//           <MenuIcon aria-label="Open Menu" />
+//         </S.IconWrapper>
+//       </MediaMatch>
+
+//       <S.LogoWrapper>
+//         <Logo hideOnMobile />
+//       </S.LogoWrapper>
+
+//       <MediaMatch greaterThan="medium">
+//         <S.MenuNav>
+//           <S.MenuLink href="#">Home</S.MenuLink>
+//           <S.MenuLink href="#">Explore</S.MenuLink>
+//         </S.MenuNav>
+//       </MediaMatch>
+
+//       <S.MenuGroup>
+//         <S.IconWrapper>
+//           <SearchIcon aria-label="Search" />
+//         </S.IconWrapper>
+//         <S.IconWrapper>
+//           <ShoppingCartIcon aria-label="Open Shopping Cart" />
+//         </S.IconWrapper>
+//         {!username && (
+//           <MediaMatch greaterThan="medium">
+//             <Link href="/sign-in" passHref>
+//               <Button as="a">Sign in</Button>
+//             </Link>
+//           </MediaMatch>
+//         )}
+//       </S.MenuGroup>
+
+//       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+//         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+//         <S.MenuNav>
+//           <S.MenuLink href="#">Home</S.MenuLink>
+//           <S.MenuLink href="#">Explore</S.MenuLink>
+
+//           {!!username && (
+//             <>
+//               <S.MenuLink href="#">My account</S.MenuLink>
+//               <S.MenuLink href="#">Wishlist</S.MenuLink>
+//             </>
+//           )}
+//         </S.MenuNav>
+
+//         {!username && (
+//           <S.RegisterBox>
+//             <Link href="/sign-in" passHref>
+//               <Button fullWidth size="large" as="a">
+//                 Log in now
+//               </Button>
+//             </Link>
+//             <span>or</span>
+//             <Link href="/sign-up" passHref>
+//               <S.CreateAccount title="Sign Up">Sign Up</S.CreateAccount>
+//             </Link>
+//           </S.RegisterBox>
+//         )}
+//       </S.MenuFull>
+//     </S.Wrapper>
+//   )
+// }
+
+// export default Menu
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,60 +1,115 @@
-import { screen, fireEvent } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+
 import Menu from '.'
 
 describe('<Menu />', () => {
   it('should render the menu', () => {
     renderWithTheme(<Menu />)
 
-    expect(
-      screen.getByRole('img', { name: /logo won games/i })
-    ).toBeInTheDocument()
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
-
-    // expect(container.firstChild).toMatchSnapshot()
   })
 
-  it('should handle the open/close menu in mobile', () => {
+  it('should handle the open/close mobile menu', () => {
     renderWithTheme(<Menu />)
 
-    // seleciona o elemento
+    // selecionar o nosso MenuFull
     const fullMenuElement = screen.getByRole('navigation', { hidden: true })
 
-    // verifica se está fechado através de atributo e estilo
+    // verificar se o menu tá escondido
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
 
-    // clicar no botão de abrir e verificar se abriu.
-    // fireEvent é um método do testing library para simular ação
+    // clicar no botão de abrir o menu e verificar se ele abriu
     fireEvent.click(screen.getByLabelText(/open menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
     expect(fullMenuElement).toHaveStyle({ opacity: 1 })
 
-    // clicar no botão de fechar e verificar se fechou.
+    // clicar no botão de fechar o menu e verificar se ele fechou
     fireEvent.click(screen.getByLabelText(/close menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 
-  it('should render the register box when user it not logged', () => {
+  it('should show register box when logged out', () => {
     renderWithTheme(<Menu />)
 
-    // utilizamos o query ao invés do get em elementos que não temos certeza
-    // de que o elemento irá existir
     expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
-    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
     expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/sign in/i)).toHaveLength(2)
   })
 
-  it('should render the wishlist and account when user it logged', () => {
-    renderWithTheme(<Menu username="thiago" />)
+  it('should show wishlight and account when logged in', () => {
+    renderWithTheme(<Menu username="will" />)
 
     expect(screen.getByText(/my account/i)).toBeInTheDocument()
     expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
-    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign in/i)).not.toBeInTheDocument()
     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
 })
+
+// import { screen, fireEvent } from '@testing-library/react'
+// import { renderWithTheme } from 'utils/tests/helpers'
+// import Menu from '.'
+
+// describe('<Menu />', () => {
+//   it('should render the menu', () => {
+//     renderWithTheme(<Menu />)
+
+//     expect(
+//       screen.getByRole('img', { name: /logo won games/i })
+//     ).toBeInTheDocument()
+//     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+//     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+//     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+
+//     // expect(container.firstChild).toMatchSnapshot()
+//   })
+
+//   it('should handle the open/close menu in mobile', () => {
+//     renderWithTheme(<Menu />)
+
+//     // seleciona o elemento
+//     const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+//     // verifica se está fechado através de atributo e estilo
+//     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+//     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+//     // clicar no botão de abrir e verificar se abriu.
+//     // fireEvent é um método do testing library para simular ação
+//     fireEvent.click(screen.getByLabelText(/open menu/i))
+//     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+//     expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+//     // clicar no botão de fechar e verificar se fechou.
+//     fireEvent.click(screen.getByLabelText(/close menu/i))
+//     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+//     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+//   })
+
+//   it('should render the register box when user it not logged', () => {
+//     renderWithTheme(<Menu />)
+
+//     // utilizamos o query ao invés do get em elementos que não temos certeza
+//     // de que o elemento irá existir
+//     expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+//     expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+//     expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+//     expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+//   })
+
+//   it('should render the wishlist and account when user it logged', () => {
+//     renderWithTheme(<Menu username="thiago" />)
+
+//     expect(screen.getByText(/my account/i)).toBeInTheDocument()
+//     expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+//     expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+//     expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+//   })
+// })
